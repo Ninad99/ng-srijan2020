96041340b40943358af2291fa7725e3076582436
Ninad Subba <ninuch7@gmail.com>
2019-11-05 04:16:37 +0530

    Workshop component added

--- src/app/components/workshop/workshop.component.ts
+++ src/app/components/workshop/workshop.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { faMapMarkerAlt, faCalendarAlt, faPhone, faMailBulk } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-workshop',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./workshop.component.scss']
 })
 export class WorkshopComponent implements OnInit {
-
+  // formData: FormGroup;
+  faMapMarker = faMapMarkerAlt;
+  faCalendarAlt = faCalendarAlt;
+  faPhone = faPhone;
+  faFacebook = faFacebook;
+  faTwitter = faTwitter;
+  faInstagram = faInstagram;
+  faMailBulk = faMailBulk;
   constructor() { }
 
   ngOnInit() {
   }
 
+  modalOpen() {
+    console.log('modal opened!');
+  }
 }
