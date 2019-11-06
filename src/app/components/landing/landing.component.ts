5cc9375ae13b8a46130e8e48b65a281a1c04a966
Ninad Subba <ninuch7@gmail.com>
2019-11-06 15:14:54 +0530

    Landing updated

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { faMailBulk } from '@fortawesome/free-solid-svg-icons';
-import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-landing',
@@ -13,7 +13,7 @@ export class LandingComponent implements OnInit {
   constructor() { }
   faCoffee = faCoffee;
   faFacebook = faFacebook;
-  faTwitter = faTwitter;
+  // faTwitter = faTwitter;
   faInstagram = faInstagram;
   faMailBulk = faMailBulk;
   // library.add(faFacebook);
